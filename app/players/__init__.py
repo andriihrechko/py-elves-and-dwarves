8d284dbdc46c0f28e6a9efeb9d8a86b011aefdcb
andriihrechko <andriihrechko250703@gmail.com>
2026-04-08 02:28:50 +0300

    Solution v1.0 Solved task.

---
+++ app/players/__init__.py
@@ -0,0 +1,5 @@
+from .player import Player
+from .dwarves.dwarf import Dwarf
+from .elves.elf import Elf
+
+__all__ = ["Player", "Dwarf", "Elf"]
---
+++ app/players/elves/druid.py
@@ -0,0 +1,20 @@
+from app.players.elves.elf import Elf
+
+
+class Druid(Elf):
+    def __init__(
+            self,
+            nickname: str,
+            musical_instrument: str,
+            favourite_spell: int
+    ) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._favourite_spell = favourite_spell
+
+    def player_info(self) -> str:
+        nickname = self.nickname
+        return (f"Druid {nickname}. {nickname} has a "
+                f"favourite spell: {self._favourite_spell}")
+
+    def get_rating(self) -> int:
+        return len(self._favourite_spell)
